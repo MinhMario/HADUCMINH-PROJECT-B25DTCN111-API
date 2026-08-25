24d978c26c69ab432d1bcb5e645db4e451d9e65b
MinhMario <minom35w@gmail.com>
2026-08-25 08:42:02 +0700

    new

--- schemas/pagination.py
+++ schemas/pagination.py
@@ -1,9 +1,10 @@
 from typing import Generic, TypeVar, List
 from pydantic import BaseModel
 
+# đại diện cho một kiểu dữ liệu cụ thể
 T = TypeVar("T")
 
-
+# generic cho class sử dụng biến kiểu đó
 class PaginatedResponse(BaseModel, Generic[T]):
     total: int
     page: int
